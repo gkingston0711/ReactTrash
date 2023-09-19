78fd1374e28a4715d374576a607a3b85e0f0ae3d
joey <jkingston90@aol.com>
2023-09-19 08:45:18 -0600

    img positioning is not correct yet

--- captcha-helper/src/Form.js
+++ captcha-helper/src/Form.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./form.css"
 
 
 export default function Form(){
+    const [randomImage, setRandomImage] = useState(null)
+
+    const generateRandomNumber = () => {
+        const min = 1; // Minimum value (inclusive)
+        const max = 1000; // Maximum value (exclusive)
+        return(Math.floor(Math.random() * (max - min) + min));
+    };
+
+    function handleClick(){
+        var myNum = generateRandomNumber()
+        var myLink =`https://picsum.photos/id/${myNum}/5000/3333`
+        console.log(myLink)
+        setRandomImage(myLink)
+    }
+
     return (
     <main>
-        <form className="form">
+        <div className="form">
             <input
                 type="text"
                 placeholder="Main topic"
@@ -17,11 +32,11 @@ export default function Form(){
                 placeholder="Sub topic"
                 className="form-input"
             />
-            <button className="form-button">
+            <button onClick={handleClick} className="form-button">
                 Get a new image
             </button>
-
-        </form>
+            <img src ={randomImage} className="captcha-image"/>
+        </div>
     </main>
     )
 }
